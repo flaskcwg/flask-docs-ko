32640b9ebe2daf13793bcab02d6ab7bad0b12dd7
blingblin-g <luvbliny@gmail.com>
2024-05-20 15:30:59 -0400

    fix: language code ko

--- docs/conf.py
+++ docs/conf.py
@@ -100,9 +100,9 @@ def setup(app):
 
 # Translation ----------------------------------------------------------
 
-locale_dirs = ['locales']   # path is example but recommended.
-gettext_compact = False     # optional.
+locale_dirs = ["locales"]  # path is example but recommended.
+gettext_compact = False  # optional.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-language
-language = ''  # update this
+language = "ko"  # update this
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_search_language
-html_search_language = ''  # update this
+html_search_language = "ko"  # update this
